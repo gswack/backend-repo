1f1c8c3c550c98a9f504049c578212352e069f62
gswack <swack.gabe@gmail.com>
2026-07-30 17:40:03 +0300

    Backend tag v4, fix Mongo URI to correct uri (with hotel-backend)

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection - using environment variable for the connection string
-mongoose.connect(process.env.MONGO_URL)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
