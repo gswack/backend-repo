ee3d31d9e42edfa142a5c0b352f0a111fbd19a4b
gswack <swack.gabe@gmail.com>
2026-08-02 21:15:02 +0300

    Backend image tag v5

--- src/server.js
+++ src/server.js
@@ -31,4 +31,3 @@ app.use("/hotels", require("./routes/hotels"));
 app.use("/reservations", require("./routes/reservations"));
 
 app.listen(3000, () => console.log("Server running on port 3000"));
-// test
