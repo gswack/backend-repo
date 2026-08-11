7b8f78a4578d714652f5441ef43838b8ece20dee
gswack <swack.gabe@gmail.com>
2026-08-11 12:59:14 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -10,29 +10,40 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-const hotelsRoute = require("./routes/hotels");
+// const hotelsRoute = require("./routes/hotels");
 
-console.log(hotelsRoute);
-console.log(typeof hotelsRoute);
-console.log(hotelsRoute instanceof Function);
-console.log(require.resolve("./routes/hotels"));
-console.log(require.resolve("./routes/reservations"));
-console.log(require("./routes/hotels"));
-console.log(require("./routes/reservations"));
+// console.log(hotelsRoute);
+// console.log(typeof hotelsRoute);
+// console.log(hotelsRoute instanceof Function);
+// console.log(require.resolve("./routes/hotels"));
+// console.log(require.resolve("./routes/reservations"));
+// console.log(require("./routes/hotels"));
+// console.log(require("./routes/reservations"));
 
 app.use(cors());
 app.use(express.json());
 
 // MongoDB connection from OpenBao secret
-const mongoUri = fs
-  .readFileSync('/vault/secrets/mongo-uri', 'utf8')
-  .trim();
+const mongoSecretPath = "/vault/secrets/mongo-uri";
+if (!fs.existsSync(mongoSecretPath)) {
+  console.error(`OpenBao secret not found: ${mongoSecretPath}`);
+  process.exit(1);
+}
+
+const mongoUri = fs.readFileSync(mongoSecretPath, 'utf8').trim();
+if (!mongoUri) {
+  console.error("OpenBao MongoDB URI is empty");
+  process.exit(1);
+}
 
 console.log("MongoDB URI loaded from OpenBao");
 
 mongoose.connect(mongoUri)
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.error("MongoDB connection failed:", err));
+  .catch((err) => {
+    console.error("MongoDB connection failed:", err);
+    process.exit(1);
+  });
 
 // Routes
 app.use("/hotels", require("./routes/hotels"));
